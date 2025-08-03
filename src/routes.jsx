c2a4ee48d518dbd5f5195142e2ce99ecd50be3f4
iamirsalimi <wonderguy2003@gmail.com>
2025-08-04 00:36:44 +0330

    debug routing

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,9 +3,9 @@ import Experiences from './Pages/Experiences/Experiences'
 import Educations from './Pages/Educations/Educations'
 
 let routes = [
-    {path:'/portfolio-redux-panel/' , element : <PersonalInfos /> },
-    {path:'/portfolio-redux-panel/Experiences' , element : <Experiences /> },
-    {path:'/portfolio-redux-panel/Educations' , element : <Educations /> }
+    {path:'/' , element : <PersonalInfos /> },
+    {path:'/Experiences' , element : <Experiences /> },
+    {path:'/Educations' , element : <Educations /> }
 ]
 
 export default routes
